839e64b80e17b879000c31e0bc734b7691a7729f
github username <github email address>
2020-03-27 14:05:55 -0400

    images

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-      <DrawWorld />
-    </div>
+        <DrawWorld />
+      </div>
     );
 
   }
--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './home.css';
 import App from './App'
 import HomeContainer from './components/HomeContainer.js'
-import { Menu} from 'semantic-ui-react'
-import {NavLink} from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
+import {NavLink } from 'react-router-dom'
 
 let token = localStorage.getItem('jwt')
 let URL = 'http://localhost:3001/'
@@ -13,10 +13,6 @@ class Home extends React.Component{
         token:token
        
     }
-
-   
-    
-
     
     render(){
         return(
@@ -30,8 +26,8 @@ class Home extends React.Component{
                 <Menu.Item link >
                     <NavLink to="/shops"exact>Shop</NavLink>
                 </Menu.Item>
-                <Menu.Item link>
-                    Heros
+                <Menu.Item link >
+                    Heroes
                 </Menu.Item>
             </Menu>
                
